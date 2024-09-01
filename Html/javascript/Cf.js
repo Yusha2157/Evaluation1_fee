87541e1e870bca3f4b268c989f16ff19f09ac703
Hustler <muhammad2157.be23@chitkara.edu.in>
2024-09-01 19:59:19 +0530

    commit

---
+++ Html/javascript/Cf.js
@@ -0,0 +1,31 @@
+let Topsearches = ["Cricket", "Marvel", "Spiderman" , "Javascript" , "Cyber Security"];
+let Imagelist = ["Images/cricket.webp", "../Images/Marvel.webp", "../Images/spiderman.webp", "../Images/Javascript.webp", "../Images/Cyber security.webp"]
+var searchlist = document.querySelector(".topsearches");
+var searchBar = document.getElementById("cfsearchbar");
+
+function showTopSearch(){
+    let length = Topsearches.length;
+    searchlist.innerHTML = `
+    <p>Top ${length} Searches: </p>
+    <ul id="Searchlist"></ul>
+    `;
+
+    const listElement = document.getElementById("Searchlist");
+    Topsearches.forEach(item => {
+        const listitem = document.createElement("li");
+        listitem.textContent = item;
+        listElement.appendChild(listitem);
+        listitem.addEventListener("click",function(){
+            searchBar.textContent = listitem;
+        })
+    })
+}
+
+let i = 0;
+var slideShow = document.querySelector(".cflpsright");
+function ImageSlide(){
+    let length = Imagelist.length;
+    slideShow.style.background = `url('${Imagelist[i]}')`;
+    i = (i + 1)%length;
+    setTimeout(ImageSlide, 3000);
+}
